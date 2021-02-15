764198b2eb2dfffb290b34d748cd4518e3fb0257
dzshn <zshn@pm.me>
2021-02-15 00:39:22 -0300

    more controls

--- src/components/Cube.vue
+++ src/components/Cube.vue
@@ -4,27 +4,16 @@
         <div id="renderWindow"></div>
         <div id="controls">
             <div v-for="(obj, index) in controls" :key="'control-'+index">
-                <label
-                    v-if="obj.type !== 'button'"
-                    :for="'input-'+index"
-                >
-                    {{ obj.title }}
-                </label>
-                <input
-                    v-if="obj.type !== 'button'"
-                    v-model="obj.value"
-                    :type="obj.type"
-                    :id="'input-'+index"
-                >
-                <button
-                    v-if="obj.type === 'button'"
-                    @click="obj.func"
-                >
-                    {{ obj.title }}
-                </button>
+                <label :for="obj">{{ obj.title }}</label>
+                <input v-model="obj.value" type="number" :id="obj">
+            </div>
+            <div>
+                <button @click="addCube">Add cube</button> /
+                <button @click="removeCube">Eat cube</button>
             </div>
         </div>
-        <p>bottom text</p>
+        <p>{{scene? scene.children.length : 'no'}}</p>
+        <h3>bottom text</h3>
     </div>
 </template>
 
@@ -34,21 +23,23 @@ import * as THREE from "three"
 export default {
     data() {
         return {
+            showAdvanced: false,
             controls: {
-                speed: {
-                    value: 50,
-                    title: "Speed: ",
-                    type: "number"
+                main: {
+                    title: "Major speed: ",
+                    value: 10
                 },
-                add: {
-                    func: this.addCube,
-                    title: "Add cube",
-                    type: "button"
+                rotate: {
+                    title: "Rotation: ",
+                    value: 1
                 },
-                remove: {
-                    func: this.removeCube,
-                    title: "Remove cube",
-                    type: "button"
+                move: {
+                    title: "Movement: ",
+                    value: 1
+                },
+                scale: {
+                    title: "Scaling: ",
+                    value: 1
                 }
             }
         }
@@ -69,9 +60,9 @@ export default {
         const renderWindow = document.getElementById('renderWindow')
         const windowWidth = renderWindow.clientWidth
         const windowHeight = renderWindow.clientHeight
-        this.scene = new THREE.Scene()
         const camera = new THREE.PerspectiveCamera(75, windowWidth / windowHeight, 0.1, 1000)
         const renderer = new THREE.WebGLRenderer()
+        this.scene = new THREE.Scene()
 
         renderer.setSize(windowWidth, windowHeight)
         renderWindow.appendChild(renderer.domElement)
@@ -84,12 +75,15 @@ export default {
             requestAnimationFrame(anim)
 
             this.scene.children.forEach(cube => {
-                cube.scale.x    += (Math.random() * 0.2 + -0.1) * (this.controls.speed.value / 100)
-                cube.scale.y    += (Math.random() * 0.2 + -0.1) * (this.controls.speed.value / 100)
-                cube.scale.z    += (Math.random() * 0.2 + -0.1) * (this.controls.speed.value / 100)
-                cube.rotation.x += (Math.random() * 0.2 + -0.1) * (this.controls.speed.value / 100)
-                cube.rotation.y += (Math.random() * 0.2 + -0.1) * (this.controls.speed.value / 100)
-                cube.rotation.z += (Math.random() * 0.2 + -0.1) * (this.controls.speed.value / 100)
+                cube.rotation.x += (Math.random() * 0.2 + -0.1) * this.controls.main.value * this.controls.rotate.value / 60
+                cube.rotation.y += (Math.random() * 0.2 + -0.1) * this.controls.main.value * this.controls.rotate.value / 60
+                cube.rotation.z += (Math.random() * 0.2 + -0.1) * this.controls.main.value * this.controls.rotate.value / 60
+                cube.position.x += (Math.random() * 0.2 + -0.1) * this.controls.main.value * this.controls.move.value   / 60
+                cube.position.y += (Math.random() * 0.2 + -0.1) * this.controls.main.value * this.controls.move.value   / 60
+                cube.position.z += (Math.random() * 0.2 + -0.1) * this.controls.main.value * this.controls.move.value   / 60
+                cube.scale.x    += (Math.random() * 0.2 + -0.1) * this.controls.main.value * this.controls.scale.value  / 60
+                cube.scale.y    += (Math.random() * 0.2 + -0.1) * this.controls.main.value * this.controls.scale.value  / 60
+                cube.scale.z    += (Math.random() * 0.2 + -0.1) * this.controls.main.value * this.controls.scale.value  / 60
             })
 
             renderer.render(this.scene, camera)
@@ -111,7 +105,7 @@ export default {
 
 #controls {
     display: grid;
-    grid-auto-flow: column;
+    grid-auto-flow: column dense;
     width: fit-content;
     margin: auto;
     gap: 5px;
@@ -121,6 +115,7 @@ export default {
     background: #fed3ea;
     border-radius: 10px;
     padding: 4px;
+    width: fit-content;
 }
 
 #controls label {
